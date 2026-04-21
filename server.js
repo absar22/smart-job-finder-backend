c197ac5ec4add4eb56b3cd09852224f1db5dc7d3
absar22 <absarahmad137@gmail.com>
2026-04-21 22:11:36 +0530

    feat: add job schema model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const connectDB = require('./config/database')
+const Job = require('./models/Jobs')
 const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
@@ -17,8 +18,29 @@ app.get('/' , (req,res) => {
  res.send('hello World')
 })
 
-app.get('/about', (req, res) => {
-    res.send('About Us')
+app.get('/test-jobs', async(req,res) => {
+    try{
+        const jobs = await Job.create({
+            title: "Software Engineer",
+            company: "Tech Corp",
+            location: "New York",
+            salary: 120000,
+            description: "Develop and maintain web applications.",
+            skills: ["JavaScript", "React", "Node.js"]
+        })
+    }catch(error){
+        console.error("Error creating job:", error);
+        res.status(500).send("Internal Server Error");
+    }
+})
+app.get('/jobs', async(req,res) => {
+    try{
+        const jobs = await Job.find();
+        res.json(jobs);
+    }catch(error){
+        console.error("Error fetching jobs:", error);
+        res.status(500).send("Internal Server Error");
+    }
 })
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
